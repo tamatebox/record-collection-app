548167171d6e38f92c86fe4edc24095468abfe71
ryota <ryota.ido.kyoto@gmail.com>
2025-03-21 15:20:53 +0900

    add format

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    complexity: ['warn', 15],
+    'max-lines-per-function': ['warn', 50],
+  },
+};
